2d1c3e977f5f863ad4509455a92556c1a58b0ded
miatran1109 <59214516+miatran1109@users.noreply.github.com>
2021-07-01 03:46:56 +0700

    persistent menu route

--- chatbot/routes/webRoutes.js
+++ chatbot/routes/webRoutes.js
@@ -1,23 +1,22 @@
-require('dotenv').config()
-const express = require('express');
-const bodyParser = require('body-parser');
-const webhookController = require('../controllers/webhookController')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const webhookController = require("../controllers/webhookController");
 var router = express.Router();
 
 let initRoutes = (app) => {
-    router.get('/', function (req, res) {
-        res.sendFile('Foodmenu.html', {root: './chatbot/restaurant'})
-    })
+  router.get("/", function (req, res) {
+    res.sendFile("Foodmenu.html", { root: "./chatbot/restaurant" });
+  });
 
-    // webhook routes
-    router.post('/webhook',  webhookController.postWebhook);
-    router.get('/webhook',  webhookController.getWebhook);
+  // webhook routes
+  router.post("/webhook", webhookController.postWebhook);
+  router.get("/webhook", webhookController.getWebhook);
 
-    router.post('/webhook/user-profile',  webhookController.messengerProfile);
-    router.post('/webhook/persistent-menu',  webhookController.persistentMenu);
+  router.post("/webhook/user-profile", webhookController.messengerProfile);
+  router.post("/webhook/persistent-menu", webhookController.persistentMenu);
 
-
-    return app.use('/', router);
-} 
+  return app.use("/", router);
+};
 
 module.exports = initRoutes;
