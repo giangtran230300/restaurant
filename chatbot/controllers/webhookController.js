f882a6211796e1012b692152a512ad5ffd2c6039
miatran1109 <59214516+miatran1109@users.noreply.github.com>
2021-07-01 05:02:05 +0700

    async

--- chatbot/controllers/webhookController.js
+++ chatbot/controllers/webhookController.js
@@ -147,7 +147,7 @@ async function callSendAPI(sender_psid, response) {
   await markMessageRead(sender_psid);
 
   // Send the HTTP request to the Messenger Platform
-  request(
+  await request(
     {
       uri: "https://graph.facebook.com/v2.6/me/messages",
       qs: { access_token: PAGE_ACCESS_TOKEN },
