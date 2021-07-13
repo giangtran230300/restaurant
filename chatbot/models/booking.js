dbdfadc70f12c9e6bbb8d89eb84b3d272d9ccb93
miatran1109 <59214516+miatran1109@users.noreply.github.com>
2021-07-13 21:56:05 +0700

    Customer model

--- chatbot/models/booking.js
+++ chatbot/models/booking.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 var bookingSchema = mongoose.Schema({
-  psid: { type: String },
-  name: { type: String },
+  psid: { type: String, required: true },
+  name: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
-  arrive_at: { type: Date },
-  phone_number: { type: String },
-  people_number: { type: String },
+  arrive_at: { type: Date, required: true },
+  phone_number: { type: String, required: true },
+  people_number: { type: String, required: true },
   note: { type: String },
-  checkin: { type: Boolean, default: false }
+  checkin: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("booking", bookingSchema);
---
+++ chatbot/models/customer.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+	CustomerName: {
+		FirstName: {
+			type: String,
+			required: true
+		},
+		LastName: {
+			type: String,
+			required: true
+		}
+	},
+	Gender: {
+		type: String,
+		required: true
+	},
+	VisitDate: [],
+	PhoneNumber: {
+		type: String,
+		required: true
+	},
+	Visited: {
+		type: Number,
+		default: 0
+	},
+
+	Point: {
+		type: Number,
+		default: 0
+	}
+});
+
+const Customer = mongoose.model('Customer', customerSchema);
+
+exports.Customer = Customer;
