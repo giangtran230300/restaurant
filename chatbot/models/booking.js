edcaa24c3509fbb6839a5517e37984b1551f950f
Sm00thiee <nhanlq2013@gmail.com>
2021-07-14 23:41:06 +0700

    change date format

--- chatbot/models/booking.js
+++ chatbot/models/booking.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
+let now =  new Intl.DateTimeFormat("en", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  day: "2-digit",
+  month: "short",
+  year: "numeric",
+}).format(Date.now());
+
 var bookingSchema = mongoose.Schema({
   psid: { type: String, required: true },
   name: { type: String, required: true },
-  created_at: { type: Date, default: Date.now },
+  created_at: { type: Date, default: now },
   arrive_at: { type: Date, required: true },
   phone_number: { type: String, required: true },
   people_number: { type: String, required: true },
