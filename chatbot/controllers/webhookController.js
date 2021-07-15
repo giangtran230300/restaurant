fdb42c63e14a82b2f797666d96ade59c50c3b760
miatran1109 <59214516+miatran1109@users.noreply.github.com>
2021-07-15 08:50:15 +0700

    Use quick replies instead of buttons

--- chatbot/controllers/webhookController.js
+++ chatbot/controllers/webhookController.js
@@ -81,7 +81,7 @@ async function handleMessage(sender_psid, received_message) {
   }
 
   // Sends the response message
-  callSendAPI(sender_psid, response);
+  chatBotService.callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
@@ -141,37 +141,6 @@ async function handlePostback(sender_psid, received_postback) {
 
   //Send the message to acknowledge the postback
   //chatBotService.callSendAPI(sender_psid, response);
-}
-
-// Sends response messages via the Send API
-async function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    recipient: {
-      id: sender_psid,
-    },
-    message: response,
-  };
-
-  await sendTypingOn(sender_psid);
-  await markMessageRead(sender_psid);
-
-  // Send the HTTP request to the Messenger Platform
-  await request(
-    {
-      uri: "https://graph.facebook.com/v2.6/me/messages",
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: request_body,
-    },
-    (err, res, body) => {
-      if (!err) {
-        console.log("message sent!");
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }
-  );
 }
 
 let messengerProfile = async (req, res) => {
