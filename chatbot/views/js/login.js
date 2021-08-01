5559231bc0707fb0d3632ae5a8ef3ffb8e3e7f86
giangtran230300 <giangtth.bi9085@st.usth.edu.vn>
2021-08-01 14:48:43 +0700

    .

--- chatbot/views/js/login.js
+++ chatbot/views/js/login.js
@@ -33,11 +33,11 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.use(express.static('./chatbot/views'));
+app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
     res.set('Access-Control-Allow-Origin', '*');
-    return res.redirect('./html/login.html');
+    return res.redirect('../html/login.html');
 })
 
 /**
@@ -57,12 +57,12 @@ app.post('/login', function (req, res) {
         }
         return res.status(200).send;
     })
-    return res.redirect( './html/customer.html');
+    return res.redirect( '../html/customer.html');
 });
 
 app.get('/logout', (req, res) => {
     req.session.destroy(function (err) {
-        res.redirect('./html/tryrest.html'); 
+        res.redirect('../html/tryrest.html'); 
     });
 
 });
